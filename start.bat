f617ab09288f3c766eb6b28499007b92ceeef4d8
easonest2 <easonest2@gmail.com>
2025-08-19 16:05:22 +1000

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ REM Start Flask web service
 start cmd /k "file.py"
 
 REM Run the other batch file (replace other_script.bat with your file name)
-start cmd /k "other_script.bat"
+start cmd /k "custom_train.bat"
 
 REM Keep window open
 pause
